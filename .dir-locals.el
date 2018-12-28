ac097a25795402798822b606d2e7b360373bed6d
Goichi Hirakawa <gooichi@gyazsquare.com>
2018-12-28 21:35:44 +0900

    refs #22 Fix .dir-locals.el for emacs 26

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,21 +1,13 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  . ((eval . (setenv "DEVELOPER_DIR"
-                     "/Applications/Xcode.app/Contents/Developer"))
-     (fill-column . 80)
-     (whitespace-style . (face lines indentation:space))
-     (eval . (set (make-local-variable 'project-dir)
-                  (file-name-directory
-                   (let ((d (dir-locals-find-file ".")))
-                     (if (stringp d) d (car d))))))
-     (eval . (set (make-local-variable 'srcroot)
-                  (expand-file-name "GSLFileTypeMappings" project-dir)))
-     (eval . (set (make-local-variable 'deproot)
+(("GSLFileTypeMappingsTests"
+  . ((objc-mode
+      . ((eval . (add-to-list
+                  'flycheck-objc-clang-framework-paths
                   (expand-file-name
-                   "GSLSynthesizeSingleton/GSLSynthesizeSingleton"
-                   project-dir)))))
+                   "Platforms/iPhoneOS.platform/Developer/Library/Frameworks"
+                   (getenv "DEVELOPER_DIR"))))))))
  (objc-mode
   . ((flycheck-objc-clang-xcrun-sdk . "iphoneos")
      (flycheck-objc-clang-arc . t)
@@ -24,10 +16,18 @@
      (flycheck-objc-clang-ios-version-min . "8.0")
      (eval . (set 'flycheck-objc-clang-include-paths
                       (list srcroot deproot)))))
- ("GSLFileTypeMappingsTests"
-  . ((objc-mode
-      . ((eval . (add-to-list
-                  'flycheck-objc-clang-framework-paths
+ (nil
+  . ((eval . (setenv "DEVELOPER_DIR"
+                     "/Applications/Xcode.app/Contents/Developer"))
+     (fill-column . 80)
+     (whitespace-style . (face lines indentation:space))
+     (eval . (set (make-local-variable 'project-dir)
+                  (file-name-directory
+                   (let ((d (or (dir-locals-find-file ".") default-directory)))
+                     (if (stringp d) d (car d))))))
+     (eval . (set (make-local-variable 'srcroot)
+                  (expand-file-name "GSLFileTypeMappings" project-dir)))
+     (eval . (set (make-local-variable 'deproot)
                   (expand-file-name
-                   "Platforms/iPhoneOS.platform/Developer/Library/Frameworks"
-                   (getenv "DEVELOPER_DIR")))))))))
+                   "GSLSynthesizeSingleton/GSLSynthesizeSingleton"
+                   project-dir))))))
